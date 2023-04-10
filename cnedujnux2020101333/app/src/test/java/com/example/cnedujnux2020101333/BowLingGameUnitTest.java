aa414d98eaaf57159fec92fcccb25b9483fa0f80
jjh1504 <j1504___w@163.com>
2023-04-11 00:40:00 +0800

    第二个测试（未通过）

--- cnedujnux2020101333/app/src/test/java/com/example/cnedujnux2020101333/BowLingGameUnitTest.java
+++ cnedujnux2020101333/app/src/test/java/com/example/cnedujnux2020101333/BowLingGameUnitTest.java
@@ -22,6 +22,14 @@ public class BowLingGameUnitTest {
         assertEquals(0,game.score());
 
     }
+    @Test
+    public void test1() throws Exception {
+        BowLingGame g = new BowLingGame();
+        for (int i = 0; i < 20; i++)
+            g.roll(1);
+        assertEquals(20, g.score());
+    }
+
 //    private BowLingGame game;
 //
 //    @Before
