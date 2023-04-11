1621213aff17085a45af6a7ba474ba576fa05575
jjh1504 <j1504___w@163.com>
2023-04-11 10:29:41 +0800

    第三次测试（未通过）

--- cnedujnux2020101333/app/src/main/java/com/example/cnedujnux2020101333/BowLingGame.java
+++ cnedujnux2020101333/app/src/main/java/com/example/cnedujnux2020101333/BowLingGame.java
@@ -1,11 +1,19 @@
 package com.example.cnedujnux2020101333;
 
 public class BowLingGame {
-    private int  score =0;
+    private int rolls[] = new int[21];
+    private int currentRoll = 0;
+
     public void roll(int pins) {
-        score += pins;
+        rolls[currentRoll++] = pins;
     }
+
     public int score() {
+        int score = 0;
+        for (int i = 0; i < rolls.length; i++)
+            score += rolls[i];
         return score;
     }
+
+
 }
